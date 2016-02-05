6274b9d9eb340436281bad29daaac5fcfe72a117
Liloubhe <liloubhe@gmail.com>
2016-02-05 09:21:58 +0100

    Adding the function "choose_new_investigator"
    It's a TUI that allows the player to choose a new investigator during the game

--- src/setup.py
+++ src/setup.py
@@ -14,12 +14,16 @@ import logging
 #-------------------------------------------------------------------------------
 
 from main import __function__
-from Class.Deck import Deck
+from Class.Deck   import Deck
 from Class.Player import Player
 
 #-------------------------------------------------------------------------------
 
 def choose_expansion(available_expansions):
+    """
+    TUI that allows the players to choose with which expansions they want to 
+    play for this game
+    """
     logging.debug("[START] " + __function__())
 
     final_expansion_list = []
@@ -35,24 +39,29 @@ def choose_expansion(available_expansions):
     _str += "\n"
 
     logging.info(_str)
-    logging.debug("[END] " + __function__())
 
+    logging.debug("[END] " + __function__())
     return final_expansion_list
 
 
 #-------------------------------------------------------------------------------
 
 def choose_investigators(expansion, nb_players):
+    """
+    TUI that allows the player to choose with which investigator they want to 
+    play for this game
+    """
     logging.debug("[START] " + __function__())
 
+    players = []
+    names_already_used = []
     investigators_list = Deck("investigators_list", expansion)
+
     print("There are " +str(investigators_list.cards_number) +
           " investigators implemented in these expansions:")
     for _iel, _elt in enumerate(investigators_list.remaining_cards):
         print(str(_iel) + ": " + _elt.name)
 
-    players = []
-    names_already_used = []
     for _iel in range(0, int(nb_players)):
         already_used = False
         while not already_used:
@@ -68,6 +77,49 @@ def choose_investigators(expansion, nb_players):
                                investigators_list.remaining_cards[int(name)]))
                 names_already_used.append(name)
                 already_used = True
+
+    logging.debug("[END] " + __function__())
+    return players
+
+
+#-------------------------------------------------------------------------------
+
+def choose_new_investigator(expansion, players, number):
+    """
+    TUI that allows the player to choose a new investigator during the game
+    """
+    logging.debug("[START] " + __function__())
+
+    names_already_used = []
+    investigators_list = Deck("investigators_list", expansion)
+    logging.info(players[number - 1].name + "has to choose a new investigator.")
+
+    print("There are " +str(investigators_list.cards_number) +
+          " investigators implemented in these expansions:")
+    for _iel, _elt in enumerate(investigators_list.remaining_cards):
+        print(str(_iel) + ": " + _elt.name)
+
+    _str = "And these investigators are already taken: "
+    for _elt in players:
+        _str += _elt.investigator.name +", "
+        names_already_used.append(_elt.investigator.name)
+    print(_str)
+
+    already_used = False
+    while not already_used:
+        name = raw_input(">> " + players[number - 1].name\
+                        + "choose investigator " + "n° ")
+        if int(name) > investigators_list.cards_number - 1:
+            print("Wrong number! Choose a number between 0 and "\
+                 + str(investigators_list.cards_number - 1) + ".")
+        else:
+            new_name = investigators_list.remaining_cards[int(name)].name
+            if new_name in names_already_used:
+                print("This investigator is already taken. Choose another one.")
+            else:
+                players[number - 1] = Player(number, 
+                                  investigators_list.remaining_cards[int(name)])
+                already_used    = True
 
     logging.debug("[END] " + __function__())
     return players
@@ -90,6 +142,7 @@ def main_setup():
     logging.info("You are " + str(nb_players) + " players\n")
 
     players = choose_investigators(chosen_expansions, nb_players)
+    players = choose_new_investigator(chosen_expansions, players, 1)
 
     logging.debug("[END] " + __function__())
 
