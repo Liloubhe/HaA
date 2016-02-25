2dfff85bd04b79104564e6e2f7416209758ee0cb
Liloubhe <liloubhe@gmail.com>
2016-02-25 16:58:55 +0100

    Update tips.py

--- tips.py
+++ tips.py
@@ -14,3 +14,4 @@ msgfmt ./i18n/fr/LC_MESSAGES/HaA.po --output-file ./i18n/fr/LC_MESSAGES/HaA.mo
 #Pour MAJ
 xgettext --language=Python --keyword=_ --from-code=UTF-8 --output=./i18n/HaA.pot `find . -name "*.py"`
 msgmerge --update --no-fuzzy-matching --backup=off ./i18n/fr/LC_MESSAGES/HaA.po ./i18n/HaA.pot
+msgfmt ./i18n/fr/LC_MESSAGES/HaA.po --output-file ./i18n/fr/LC_MESSAGES/HaA.mo
